d5396bf6f899ff3659df18ba33eaa3dddc9becda
dilmurod <dilmurod@gmail.com>
2023-04-20 20:58:08 +0500

    ch

--- pages/views.py
+++ pages/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home_view(request):
-    return HttpResponse("Hello world")
+    return render(request, "pages/index.html")
 
+
+def shop_view(request):
+    return render(request, "pages/shop.html")
