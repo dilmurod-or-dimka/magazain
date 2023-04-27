e24f63367e13a8b6d51f0e33b5bc4a184965ce8f
dilmurod <dilmurod@gmail.com>
2023-04-27 20:58:05 +0500

    adder products

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Category, Brand
+from .models import Category, Brand, Product, ProductImage
+
 
 # Register your models here.
 
@@ -15,3 +16,18 @@ class CategoryAdmin(admin.ModelAdmin):
 class BrandAdmin(admin.ModelAdmin):
     list_display = ("pk", "title", "slug")
     list_display_links = ("pk", "title")
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "price", "quantity", "slug", "is_available", "category", "category")
+    list_display_links = ("pk", "title")
+    list_filter = ("brand", "category")
+    prepopulated_fields = {"slug": ("title",)}
+    inlines = [ProductImageInline]
+    list_editable = ("is_available",)
