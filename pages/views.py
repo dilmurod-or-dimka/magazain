6ff3c0885510a63110f6a30969f2218789446dba
dilmurod <dilmurod@gmail.com>
2023-05-11 21:04:15 +0500

    added pagination

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from .models import Product, Category
-
-
+from django.core.paginator import Paginator
 # Create your views here.
+
 
 def home_view(request):
     products = Product.objects.all()[:9]
@@ -14,8 +14,11 @@ def home_view(request):
 
 def shop_view(request):
     products = Product.objects.all()
+    paginator = Paginator(products, 1)
+    page = request.GET.get("page")
+    result = paginator.get_page(page)
     context = {
-        "products": products
+        "products": result
     }
     return render(request, "pages/shop.html", context)
 
@@ -23,8 +26,12 @@ def shop_view(request):
 def category_products(request, slug):
     category = Category.objects.get(slug=slug)
     products = Product.objects.filter(category=category)
+    paginator = Paginator(products, 1)
+    page = request.GET.get("page")
+    result = paginator.get_page(page)
+
     context = {
-        "products": products
+        "products": result
     }
     return render(request, "pages/shop.html", context)
 
